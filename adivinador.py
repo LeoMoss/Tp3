33a7657e227dff4d34685a0d3aae2d4118a25049
LeoMoss <leonardo.mosqueira88@gmail.com>
2021-09-08 21:14:03 -0300

    Tercer Commit, Programa Terminado

--- adivinador.py
+++ adivinador.py
@@ -1,1 +1,18 @@
 # Primer commit, clasico programa de adivina el numero  Ramdom entre 1-30
+import random
+ 
+secret = random.randint(1, 30)
+guess = 0
+tries = 0
+ 
+print ("Adivina el Numero Secreo")
+print ("El número es del 1 al 30, ¡solo tienes 6 posibilidades!")
+ 
+print ("¿Cuál es el número que adivinaste?")
+guess = input()
+
+if int(guess) == secret:
+	print ("¡Eres increíble! ¡Felicitaciones, lo has adivinado!")
+else:
+	print ("Has adivinado mal ¡juguemos de nuevo la próxima vez!")
+	print ("Mi número secreto es:", secret)
