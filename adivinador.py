073ee89daa66a051b7926e90338e51537b733a2b
EstefaniaFerraro <estefania.ferrro@gmail.com>
2021-09-09 23:09:27 -0300

    Agrego que el jugador tenga 6 intentos

--- adivinador.py
+++ adivinador.py
@@ -8,11 +8,16 @@ tries = 0
 print ("Adivina el Numero Secreo")
 print ("El número es del 1 al 30, ¡solo tienes 6 posibilidades!")
  
-print ("¿Cuál es el número que adivinaste?")
-guess = input()
-
+while int(guess) != secret and tries < 6:
+	print ("¿Cuál es el número que adivinaste?")
+	guess = input()
+	if int(guess) < secret:
+		print ("¡El número es demasiado pequeño! ¡Adivina de nuevo!")
+	elif int(guess) > secret:
+		print ("¡El número es demasiado grande! ¡Adivina de nuevo!")
+		tries = tries + 1
 if int(guess) == secret:
 	print ("¡Eres increíble! ¡Felicitaciones, lo has adivinado!")
 else:
-	print ("Has adivinado mal ¡juguemos de nuevo la próxima vez!")
+	print ("Has adivinado mal 6 veces, ¡juguemos de nuevo la próxima vez!")
 	print ("Mi número secreto es:", secret)
